f341140ed3e3abedb8869ddd1e04563bd69eebd5
ETamez173 <56414835+ETamez173@users.noreply.github.com>
2021-03-08 16:34:17 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+// Getting User Input | C# | Tutorial 8
+
+namespace CSharpExercises_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello");
+            Console.Write("Enter your name:  ");
+          
+            string name = Console.ReadLine();
+
+            Console.Write("Enter your age:  ");
+
+            string age = Console.ReadLine();
+
+            Console.WriteLine("Hello " + name + " you are " + age + " years old");
+
+            Console.ReadLine();
+        }
+    }
+}
